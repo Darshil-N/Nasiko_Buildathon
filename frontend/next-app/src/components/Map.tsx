26ffe5569bf34b72d33d7b7c33c149f7c4c42041
Anonymous-7777 <darshilnathwani@gmail.com>
2026-09-20 15:07:45 +0530

    [B] Swap map tile provider to Esri Light Gray Base (No API key required)

--- frontend/next-app/src/components/Map.tsx
+++ frontend/next-app/src/components/Map.tsx
@@ -27,8 +27,8 @@ export default function Map({ center, zoom }: MapProps) {
   return (
     <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} style={{ height: "100%", width: "100%", borderRadius: "24px" }} zoomControl={false}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
       />
       {polygons.map(p => (
         <Polygon
